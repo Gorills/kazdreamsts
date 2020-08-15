94c27de96e51ad6994bbe72d6a45130f9372e03a
Gorills <gorivanickiy@gmail.com>
2020-08-15 10:48:23 +0700

    form

--- js/theme.js
+++ js/theme.js
@@ -434,3 +434,25 @@
 
 
 })(jQuery)
+
+$('body').on('click', '.password-control', function(){
+    if ($('.password-input').attr('type') == 'password'){
+        $(this).addClass('view');
+        $('.password-input').attr('type', 'text');
+    } else {
+        $(this).removeClass('view');
+        $('.password-input').attr('type', 'password');
+    }
+    return false;
+});
+
+$('body').on('click', '.password-control-2', function(){
+    if ($('.password-input-2').attr('type') == 'password'){
+        $(this).addClass('view');
+        $('.password-input-2').attr('type', 'text');
+    } else {
+        $(this).removeClass('view');
+        $('.password-input-2').attr('type', 'password');
+    }
+    return false;
+});
